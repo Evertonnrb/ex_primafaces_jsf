ebc979e51a596c6d2a81feadc3d59cc49acf92f9
everton <everton.nrb@hotmail.com>
2016-11-07 22:25:33 -0300

    Corrigindo bug's

--- src/java/nrb/com/br/bean/CarroBean.java
+++ src/java/nrb/com/br/bean/CarroBean.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nrb.com.br.bean;
 
-/**
- *
- * @author Everton
- */
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import nrb.com.br.entity.Carro;
+
+@ManagedBean
+@SessionScoped
 public class CarroBean {
-    
+
+    private Carro carro = new Carro();
+    private List<Carro> carros = new ArrayList();
+
+    public void adicionar() {
+        carros.add(carro);
+        carro = new Carro();
+
+    }
+
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+
+    public List<Carro> getCarros() {
+        return carros;
+    }
+
+    public void setCarros(List<Carro> carros) {
+        this.carros = carros;
+    }
 }
